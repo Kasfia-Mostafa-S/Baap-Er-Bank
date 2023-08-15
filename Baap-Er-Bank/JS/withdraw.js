478ccde7fce8eab271b53a6afc416e9cf04980eb
SwornaCode <kasfiasworna03@gmail.com>
2023-08-15 15:55:52 +0600

    bank

--- Baap-Er-Bank/JS/withdraw.js
+++ Baap-Er-Bank/JS/withdraw.js
@@ -6,27 +6,40 @@ document.getElementById('btn-withdraw').addEventListener('click',function(){
   const newWithdrawAmountString = withdrawField.value;
   const newWithdrawAmount = parseFloat(newWithdrawAmountString);
 
+  // step-8 clear withdraw field
+  withdrawField.value ='';
+
+  if(isNaN(newWithdrawAmount)){
+    alert('Please provide a valid number');
+    return;
+  }
+
   // step-3 get the previous amount
   const withdrawTotalElement = document.getElementById('withdraw-total');
   const previousWithdrawTotalString = withdrawTotalElement.innerText;
   const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
-
-  // step-4 total withdraw amount
-  const currentWithdraw = previousWithdrawTotal + newWithdrawAmount;
-
-   // set-5 deposit 
-   withdrawTotalElement.innerText = currentWithdraw;
 
    // step-6 get balance current total
    const balanceTotalElement = document.getElementById('balance-total');
    const previousBalanceTotalString = balanceTotalElement.innerText;
    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
+   
+
+   if(newWithdrawAmount > previousBalanceTotal){
+    alert('Not enough money');
+    return;
+   }
+
+   // step-4 total withdraw amount
+   const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+   // set-5 deposit 
+   withdrawTotalElement.innerText = currentWithdrawTotal;
+
    // step-7 calculate current total balance
    const currentBalanceTotal = previousBalanceTotal - newWithdrawAmount;
    balanceTotalElement.innerText = currentBalanceTotal;
 
-   //  step-8 clear withdraw field
-   withdrawField.value = '';
+   
 
 })
