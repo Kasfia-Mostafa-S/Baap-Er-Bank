3200cf19d37351ae51fda955fd6039bcc883c600
SwornaCode <kasfiamostafa03@gmail.com>
2023-08-16 21:46:43 +0600

    video-3

--- Baap-Er-Bank-recap/JS/deposit.js
+++ Baap-Er-Bank-recap/JS/deposit.js
@@ -6,10 +6,20 @@ document.getElementById("btn-deposit").addEventListener('click',function(){
    
  depositField.value ='';
 
- const depositTotalElement = document.getElementById('deposit-total');
+ const depositTotalElement = document.getElementById('balance-total');
  const previousDepositTotalString = depositTotalElement.innerText;
  const previousDepositTotal = parseFloat(previousDepositTotalString);
 
  const newDepositTotal = previousDepositTotal + newDepositAmount;
  depositTotalElement.innerText = newDepositTotal;
+
+
+//  balance part
+const balanceTotalElement = document.getElementById('balance-total');
+const previousBalanceTotalString = balanceTotalElement.innerText;
+const previousBalanceTotalAmount = parseFloat(previousDepositTotalString); 
+const newBalanceTotal = previousBalanceTotalAmount + newDepositAmount;
+balanceTotalElement.innerText = newBalanceTotal;
+
+
 })
